b15d639a9521b5bfd8e5769b2471ed99acec21be
Daniel Ruiz <daniru2770@gmail.com>
2025-06-02 14:34:21 -0500

    Clase y controlador Ahorro.

---
+++ Clases/clsAhorro.cs
@@ -0,0 +1,100 @@
+﻿using Natillera.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace Natillera.Clases
+{
+    public class clsAhorro
+    {
+        private NatilleraDBEntities dbNatillera = new NatilleraDBEntities();
+        public Ahorro ahorro { get; set; }
+
+        public string Insertar()
+        {
+            try
+            {
+                dbNatillera.Ahorroes.Add(ahorro);
+                dbNatillera.SaveChanges();
+                return "Ahorro insertado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al insertar el ahorro: " + ex.Message;
+            }
+        }
+
+        public string Actualizar()
+        {
+            try
+            {
+                Ahorro ah = Consultar(ahorro.id);
+                if (ah == null)
+                    return "El ahorro con el ID ingresado no existe";
+
+                dbNatillera.Ahorroes.AddOrUpdate(ahorro);
+                dbNatillera.SaveChanges();
+                return "Ahorro actualizado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al actualizar el ahorro: " + ex.Message;
+            }
+        }
+
+        public List<Ahorro> ConsultarTodos()
+        {
+            return dbNatillera.Ahorroes.OrderBy(a => a.Fecha).ToList();
+        }
+
+        public Ahorro Consultar(long id)
+        {
+            return dbNatillera.Ahorroes.FirstOrDefault(a => a.id == id);
+        }
+
+        public List<Ahorro> ConsultarPorCliente(int idCliente)
+        {
+            return dbNatillera.Ahorroes
+                .Where(a => a.idCliente == idCliente)
+                .OrderByDescending(a => a.Fecha)
+                .ToList();
+        }
+
+        public string Eliminar()
+        {
+            try
+            {
+                Ahorro ah = Consultar(ahorro.id);
+                if (ah == null)
+                    return "El ahorro no existe";
+
+                dbNatillera.Ahorroes.Remove(ah);
+                dbNatillera.SaveChanges();
+                return "Ahorro eliminado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al eliminar el ahorro: " + ex.Message;
+            }
+        }
+
+        public string Eliminar(long id)
+        {
+            try
+            {
+                Ahorro ah = Consultar(id);
+                if (ah == null)
+                    return "El ahorro no existe";
+
+                dbNatillera.Ahorroes.Remove(ah);
+                dbNatillera.SaveChanges();
+                return "Ahorro eliminado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al eliminar el ahorro: " + ex.Message;
+            }
+        }
+    }
+}
---
+++ Controllers/AhorroController.cs
@@ -0,0 +1,57 @@
+﻿using Natillera.Clases;
+using Natillera.Models;
+using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Natillera.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    [RoutePrefix("api/Ahorros")]
+    [Authorize]
+    public class AhorroController : ApiController
+    {
+        clsAhorro gestor = new clsAhorro();
+
+        [HttpPost]
+        [Route("Insertar")]
+        public IHttpActionResult Insertar([FromBody] Ahorro ahorro)
+        {
+            gestor.ahorro = ahorro;
+            var mensaje = gestor.Insertar();
+            return Ok(mensaje);
+        }
+
+        [HttpPut]
+        [Route("Actualizar")]
+        public IHttpActionResult Actualizar([FromBody] Ahorro ahorro)
+        {
+            gestor.ahorro = ahorro;
+            var mensaje = gestor.Actualizar();
+            return Ok(mensaje);
+        }
+
+        [HttpGet]
+        [Route("Listar")]
+        public IHttpActionResult ConsultarTodos()
+        {
+            var lista = gestor.ConsultarTodos();
+            return Ok(lista);
+        }
+
+        [HttpGet]
+        [Route("PorCliente")]
+        public IHttpActionResult ConsultarPorCliente(int idCliente)
+        {
+            var lista = gestor.ConsultarPorCliente(idCliente);
+            return Ok(lista);
+        }
+
+        [HttpDelete]
+        [Route("Eliminarxid")]
+        public IHttpActionResult Eliminar(int id)
+        {
+            var mensaje = gestor.Eliminar(id);
+            return Ok(mensaje);
+        }
+    }
+}
