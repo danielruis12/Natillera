7d07d94cd46d9fc1e60d1bc9e163aa2de00642a5
SebastianITM <sebastiancarvajal210993@correo.itm.edu.co>
2025-06-05 18:42:10 -0500

    Se agrega el Eliminar y el Elminar por id, asi como el consultar todos de la clase proveedor

--- Clases/clsProveedor.cs
+++ Clases/clsProveedor.cs
@@ -44,9 +44,49 @@ namespace Natillera.Clases
             }
         }
 
+        public List<Proveedor> ConsultarTodos()
+        {
+            return dbNatillera.Proveedors.OrderBy(p => p.Nombre).ToList();
+        }
+
         public Proveedor Consultar(int id)
         {
             return dbNatillera.Proveedors.FirstOrDefault(c => c.id == id);
+        }
+        public string Eliminar()
+        {
+            try
+            {
+                Proveedor pro = Consultar(proveedor.id);
+                if (pro == null)
+                    return "El proveedor no existe";
+
+                dbNatillera.Proveedors.Remove(pro);
+                dbNatillera.SaveChanges();
+                return "Proveedor eliminado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al eliminar el proveedor: " + ex.Message;
+            }
+        }
+
+        public string Eliminar(int id)
+        {
+            try
+            {
+                Proveedor pro = Consultar(id);
+                if (pro == null)
+                    return "El proveedor no existe";
+
+                dbNatillera.Proveedors.Remove(pro);
+                dbNatillera.SaveChanges();
+                return "Proveedor eliminado correctamente";
+            }
+            catch (Exception ex)
+            {
+                return "Error al eliminar el proveedor: " + ex.Message;
+            }
         }
 
     }
