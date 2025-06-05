1e38e2581bd15990b57035b94a97e573fa023504
Daniel Ruiz <daniru2770@gmail.com>
2025-06-05 15:33:22 -0500

    Autorización Login.

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -10,7 +10,8 @@ namespace Natillera.Controllers
     [RoutePrefix("api/login")]
     // [AllowAnonymous]: Directiva para que el servicio no requiera de autenticación.
     // [Authorize]: Directiva para que el servicio necesite de autenticación y un token para que se pueda procesar.
-    [AllowAnonymous]
+    //[AllowAnonymous]
+    [Authorize]
     public class LoginController : ApiController
     {
         [HttpPost]
